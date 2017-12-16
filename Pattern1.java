6786617fd4b9504720b773240390fc148c074443
madhuelango <34329402+madhuelango@users.noreply.github.com>
2017-12-16 16:40:06 +0530

    pattern*

--- Pattern1.java
+++ Pattern1.java
@@ -1,19 +1,36 @@
-import java.util.Scanner;
 class Pattern1
 {
 public static void main(String args[])
 {
-	Scanner s=new Scanner(System.in);
-	int n=s.nextInt();
-	
-		for(int i=1;i<=n;i++)
+	int m=0;
+	for(int i=10;i>0;i=i-2)
+	{
+		for(int j=1;j<i/2;j++)
 		{
-			for(int j=1;j<=i;j++)
-			{
-			System.out.print("*");
-			}
-			System.out.println();	
+			System.out.print(" ");
+			m=j;
 		}
-	
+		for(int k=0;k<9-2*m;k++)
+		{
+			System.out.print("*");
+		}
+		System.out.println();
+		m=0;
+
+	}
+	for(int i=1;i<10;i=i+2)
+	{
+		for(int j=0;j<=i/2;j++)
+		{
+			System.out.print(" ");
+			m=j;
+		}
+		for(int k=7-2*m;k>0;k--)
+		{
+			System.out.print("*");
+		}
+		System.out.println();
+
+	}
 }
 }
